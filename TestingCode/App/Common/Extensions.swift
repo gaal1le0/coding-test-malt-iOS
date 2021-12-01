d9b47c30182cbb8a16c20ad84ccb59a29889ae00
Manu <e-eeaz@gft.com>
2021-12-01 08:37:51 +0100

    Created extension in table to perform data

--- TestingCode/App/Common/Extensions.swift
+++ TestingCode/App/Common/Extensions.swift
@@ -6,6 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 /// Extending String to conform to protocol Error
 extension String: Error { }
+
+extension UITableView {
+    
+    /// configurate table view according to an state
+    func setBackgound(to state: MainViewState) {
+        switch state {
+        case .loading:
+            let spinnerTable = UIActivityIndicatorView(style: .large)
+            spinnerTable.tintColor = UIColor.black
+            spinnerTable.hidesWhenStopped = true
+            spinnerTable.startAnimating()
+            self.backgroundView = spinnerTable
+        case .error(let message):
+            let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+            messageLabel.text = message.localizedDescription
+            messageLabel.textColor = .black
+            messageLabel.numberOfLines = 0
+            messageLabel.textAlignment = .center
+            messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+            messageLabel.sizeToFit()
+            self.backgroundView = messageLabel
+            self.backgroundView?.backgroundColor = backgroundColor
+        case .data:
+            backgroundView = nil
+        }
+    }
+    
+}
